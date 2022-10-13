d8a977ad4d3d187e6a810d36873373dd77339525
Kim <xzsa18@naver.com>
2022-10-13 22:08:51 +0900

    어댑터 변수 추가, 리스트뷰의 어댑터로써 연결

--- app/src/main/java/com/example/listviewpractice/MainActivity.kt
+++ app/src/main/java/com/example/listviewpractice/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.listviewpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.listviewpractice.adapters.StudentAdapter
 import com.example.listviewpractice.datas.Student
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val mStudentList = ArrayList<Student>()
+
+    lateinit var mAdapter : StudentAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +23,10 @@ class MainActivity : AppCompatActivity() {
         mStudentList.add( Student("김일성",1950))
         mStudentList.add( Student("푸틴",1940))
         mStudentList.add( Student("바이든",1930))
+
+        mAdapter = StudentAdapter(this, R.layout.student_list_item, mStudentList)
+
+        studentListView.adapter = mAdapter
+
     }
 }
