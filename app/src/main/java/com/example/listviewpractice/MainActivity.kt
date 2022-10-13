986828c0b433a3f4bc2c93fdeaf718280240b791
Kim <xzsa18@naver.com>
2022-10-13 21:59:49 +0900

    학생 목록 ArrayList 생성, 실제 목록 추가

--- app/src/main/java/com/example/listviewpractice/MainActivity.kt
+++ app/src/main/java/com/example/listviewpractice/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.listviewpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.listviewpractice.datas.Student
 
 class MainActivity : AppCompatActivity() {
+
+    val mStudentList = ArrayList<Student>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mStudentList.add( Student("김철수",1988))
+        mStudentList.add( Student("김영희",1999))
+        mStudentList.add( Student("김정은",1971))
+        mStudentList.add( Student("김일성",1950))
+        mStudentList.add( Student("푸틴",1940))
+        mStudentList.add( Student("바이든",1930))
     }
 }
